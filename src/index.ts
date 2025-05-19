c448e59d23a843329c9e0aadd69341457fb95776
deepak <deepak@blend.to>
2025-05-19 16:25:01 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { SQSExtendedBridge } from "./bridge";
-export default SQSExtendedBridge;
+import SQSExtendedClient from "./client";
+export default SQSExtendedClient;
 
 export { SQSExtendedClientException } from "./exceptions";
 export { S3Pointer, SqsExtendedClientOptions } from "./types";
